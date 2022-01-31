4358fb0fb929fa5e6aba833eac04b396ff138ae0
codecaiine <akabrouyannickn@gmail.com>
2022-01-31 07:45:57 +0000

    Import redux

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,11 @@
 import React from "react"
-import PropTypes from "prop-types"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import PropTypes from "prop-types"
+import configureStore from '../redux/configureStore'
 import Greeting from './Greeting'
+
+const store = configureStore();
 
 class App extends React.Component {
   render () {
