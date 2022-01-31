ea52d10785910b6342bf11d7628bdb03158bd6f9
codecaiine <akabrouyannickn@gmail.com>
2022-01-31 00:44:15 +0000

    Generate react component

---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Greeting extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Greeting
