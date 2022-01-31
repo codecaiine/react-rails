86a36be53836051f7f8e4006a21d753a0a179242
codecaiine <akabrouyannickn@gmail.com>
2022-01-31 00:45:28 +0000

    Add proptype

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -4,9 +4,13 @@ class Greeting extends React.Component {
   render () {
     return (
       <React.Fragment>
+        Greeting: {this.props.greeting}
       </React.Fragment>
     );
   }
 }
 
+Greeting.propTypes = {
+  greeting: PropTypes.string
+};
 export default Greeting
